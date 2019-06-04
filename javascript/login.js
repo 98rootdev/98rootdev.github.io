88b948adb072f3bbdbeb48dec4582717bac04384
Kevin <kevbra98@gmail.com>
2019-06-04 19:23:39 +0200

    Updates to login page

--- javascript/login.js
+++ javascript/login.js
@@ -1,8 +1,16 @@
-var username = document.getElementById("username");
-var password = document.getElementById("password");
 var users = [];
 
 
-function login(){
-    if()
+function loginValid(){
+    if(document.getElementById("username") !="" || document.getElementById("password") !="" && document.getElementById("username") = users[].username || document.getElementById("password") = users[].password
+       
+       
+        //&& both inputs is part of same object){
+        login = true;
+        //load index.html;
+       
+    }else{
+        login = false;
+        alert("Wrong username or password, please try again.");
+    }
 }
