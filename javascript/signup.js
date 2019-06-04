f13531493676ced713aab93ebaca9d535f5e7749
Kevin <kevbra98@gmail.com>
2019-06-04 18:13:05 +0200

    Css update

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,21 +1,28 @@
 var users = []
 
 function createUser(){
-    if(document.value != ""){
+    if (document.getElementById("firstname").value !="" || document.getElementById("lastname").value) !="" || document.getElementById("username").value !="" || document.getElementById("email").value != "" || document.getElementById("password").value !="" || document.getElementById("phonenumber").value !=""{
+    
     var firstname = document.getElementById("firstname").value;
     var lastName = document.getElementById("lastname").value;
     var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;    
     var password = document.getElementById("password").value;
-    var phonenumber = document.getElementById("phonenumber").value
+    var phonenumber = document.getElementById("phonenumber").value;
         
     users.push({
     firstname: firstname,
     lastname: lastname,
     username: username,
+    email: email,
     password: password,
     phonenumber: phonenumber
+
 });
+        console.log(users[0])
    
     }else{alert("Registration failed, please try again.")};
 
 };
+
+
