8e883a5305cd0a42f502a6826560ae22b46d51f7
Kevin <kevbra98@gmail.com>
2019-06-04 17:09:03 +0200

    Update to signup js

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,43 +1,21 @@
-var firstname = document.getElementById("firstname");
-var lastname = document.getElementById("lastname");
-var username = document.getElementById("username");
-var password = document.getElementById("password");
-var phonenumber = document.getElementById("phonenumber");
-var submitBtn = document.getElementById("submitBtn");
-var users = [];
-
-//Arrays which contain registrered users.
-var regfname = ["Kev"];
-var reglname = ["B"];
-var regpassword = ["psswd"];
-var regphonenumber = ["911"];
-                   
+var users = []
 
 function createUser(){
-users.push[
-    username: ,
-    password: ,
-    firstname: ,
-    lastname; ,
-    phonenumber: ,
-]
-}
+    if(document.value != ""){
+    var firstname = document.getElementById("firstname").value;
+    var lastName = document.getElementById("lastname").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var phonenumber = document.getElementById("phonenumber").value
+        
+    users.push({
+    firstname: firstname,
+    lastname: lastname,
+    username: username,
+    password: password,
+    phonenumber: phonenumber
+});
+   
+    }else{alert("Registration failed, please try again.")};
 
-/*
-function createUser(){
-    if(firstname.value != "", lastname.value != "", username.value != "", password.value != "", phonenumber.value != "", occupation.value != ""){
-        regfname.push(firstname.value), reglname.push(lastname.value), users.push(username.value), regpassword.push(password.value), regphonenumber.push(phonenumber.value);
-        return false;
-    }else{
-        alert("Registration failed, please try again.");
-    }
-}
-
-submitBtn.addEventListener("click", function test(){
-    regfname.push("Hei"));
 };
-     
-                           
-function createUser(){
-    oninput
-}     */                      
