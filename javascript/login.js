42755b271553918b8ac96817f8515935d6ce3435
Kevin <kevbra98@gmail.com>
2019-06-06 20:15:17 +0200

    Lots of commenting done

--- javascript/login.js
+++ javascript/login.js
@@ -1,37 +1,37 @@
-/**
- *
- * Checks whether or not the given username and password is valid for some user.
- *
- * @returns
- */
-function login() {
+// Function for the login page, which checks if both the username and password is correct.
 
+function login() {
+    /* Getting the values from the input typed by the user, and declaring the variables
+for them here so we can use them later in the code without typing it in every time */ 
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    // If both username and password is NOT empty:
+    // Checks if there is any input at all, if not, it alerts you that some fields can't be empty.
     if (!(username == "" && password == "")) {
 
-        // Look through array of all users to check if given credentials are valid for any user.
+        /* Loops through the array containing users to check if given credentials are
+            valid for any user.*/
         for (let i = 0; i < window.users.length; i++) {
+            
+            // We get the info from signup.js file, which contains our data
 
             let validUsername = window.users[i].username;
             let validPassword = window.users[i].password;
 
             if (username == validUsername && password == validPassword) {
 
-                // The username and the password is correct.
+                // If both the username and password are correct. We will be redirected to the mainpage.
                 window.location.replace("../pages/index.html");
                 return true;
             }
         }
 
-        // The username or password is incorrect.
+        // If username or password is incorrect, you will be alerted with this.
         alert("Wrong username or password, please try again.");
         return false;
     }
 
-    // Fields are empty.
+    // If any of the fields are empty.
     alert("Fields cannot be empty.");
     return false;
 }
